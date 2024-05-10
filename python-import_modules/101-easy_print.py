b8e9c6180e4642b470405b194640dfae64181384
jydzip <dckjeremy.zip@gmail.com>
2024-05-10 14:17:41 +0200

    Add task 101

--- python-import_modules/101-easy_print.py
+++ python-import_modules/101-easy_print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-from os import open, close, write, O_WRONLY
-
-if __name__ == "__main__":
-    fd = open('/dev/stdout', O_WRONLY)
-    write(fd, b"#pythoniscool\n")
-    close(fd)
+import os; os.write(1, b"#pythoniscool\n")
