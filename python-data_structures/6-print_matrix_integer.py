13aa2c7b6047d6ea499a3303a60131d949fcd6ff
jydzip <dckjeremy.zip@gmail.com>
2024-05-13 11:09:24 +0200

    Add task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        _size = len(row)
+        _i = 0
+        for column in row:
+            _i += 1
+            print(
+                    "{:d}".format(column),
+                    end="\n" if _i == _size else " "
+            )
