aca444001a62d7e2d850e1c7d59205a6bfbc7ff9
jydzip <dckjeremy.zip@gmail.com>
2024-05-13 11:34:43 +0200

    Fix task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     _check = None
     for _int in my_list:
-        if _check == None or _int > _check:
+        if _check is None or _int > _check:
             _check = _int
     return _check
