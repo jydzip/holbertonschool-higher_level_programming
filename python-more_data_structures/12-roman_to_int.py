fcb698d9bef1c0389975c2a80c0cd6b770c0df5b
jydzip <dckjeremy.zip@gmail.com>
2024-05-14 09:51:45 +0200

    Fix task 12 if not stringé

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     result = 0
-    if not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return result
 
     _romans = {
