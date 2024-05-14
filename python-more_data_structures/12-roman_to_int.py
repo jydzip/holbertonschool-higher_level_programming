d57e7c2ba712e64e1b58eaa991c25a53a8aac978
jydzip <dckjeremy.zip@gmail.com>
2024-05-14 10:14:18 +0200

    Fix task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,6 +13,14 @@ def roman_to_int(roman_string):
             "D": 500,
             "M": 1000
     }
-    for letter in roman_string:
+    for i in range(len(roman_string)):
+        letter = roman_string[i]
+        try:
+            next_letter = roman_string[i + 1]
+        except:
+            next_letter = None
+
+        if next_letter in ["V", "X"]:
+            result += -2
         result += _romans.get(letter, 0)
     return result
