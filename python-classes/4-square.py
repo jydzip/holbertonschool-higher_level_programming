b9c91d09c5e64cc5fdb67218fad5336c24351659
jydzip <dckjeremy.zip@gmail.com>
2024-05-16 21:41:31 +0200

    Fix task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,16 +10,22 @@ class Square:
         Args:
             size (int, optional): Size of the square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         """Get the current square area."""
         return self.__size * self.__size
 
+    @property
     def size(self):
         """Get the size of square."""
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter of the size field."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
