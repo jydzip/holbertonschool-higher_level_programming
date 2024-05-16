0bd069dde07d52d5762dbc512657e5718a542dff
jydzip <dckjeremy.zip@gmail.com>
2024-05-16 21:45:47 +0200

    Fix task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a square."""
+
+
 class Square:
     """Defines a square."""
     def __init__(self, size=0):
@@ -7,11 +10,7 @@ class Square:
         Args:
             size (int, optional): Size of the square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         """Get the current square area."""
