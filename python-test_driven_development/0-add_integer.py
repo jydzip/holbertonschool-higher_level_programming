6822d86132caf2f549e24b4ed395c59624f1c685
jydzip <dckjeremy.zip@gmail.com>
2024-05-20 15:36:42 +0200

    Fix 0-add

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,9 +13,9 @@ def add_integer(a, b=98):
         Return:
             (int): Resultat of addition.
     """
-    if not isinstance(a, (int, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
