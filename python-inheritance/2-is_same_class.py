782c480276f841155f87855efab84f02291c6c06
jydzip <dckjeremy.zip@gmail.com>
2024-05-23 10:59:57 +0200

    Add task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Function file for is_same_class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Check if object if exactly an instance of the specified class.
+
+        Arguments:
+            obj (class): Class to test.
+            a_class (type): Type to check obj.
+        Return:
+            (bool) Resultat.
+    """
+    if isinstance(obj, a_class):
+        _dir_obj = dir(obj)
+        _dir_class = dir(a_class)
+        if _dir_obj == _dir_class:
+            return True
+    return False
