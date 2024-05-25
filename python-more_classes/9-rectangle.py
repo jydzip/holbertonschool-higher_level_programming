b16a30bbab87bc5291eb581e2e2c1a2e02b746cf
jydzip <dckjeremy.zip@gmail.com>
2024-05-25 18:23:08 +0200

    Try fix task 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -72,11 +72,9 @@ class Rectangle:
         """
             Returns the rectangle perimeter.
         """
-        width = self.width
-        height = self.height
-        if width == 0 or height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
-        return (width + height) * 2
+        return (self.width + self.height) * 2
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
@@ -89,21 +87,25 @@ class Rectangle:
         return rect_1
 
     def __str__(self):
+        if self.height == 0 or self.width == 0:
+            return ""
         text = ""
-        if self.height != 0 and self.width != 0:
-            for i in range(self.height):
-                text += "{:s}{:s}".format(
-                    self.width * str(self.print_symbol),
-                    "" if (i + 1) == self.height else "\n"
-                )
+        for row in range(self.height):
+            for column in range(self.width):
+                try:
+                    text += str(self.print_symbol)
+                except Exception:
+                    text += type(self).print_symbol
+            if row < self.height - 1:
+                text += "\n"
         return text
 
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.width, self.height)
 
     def __del__(self):
-        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @classmethod
     def square(cls, size=0):
