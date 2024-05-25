0358e8c2909b7789eb016095558de017ea91602c
jydzip <dckjeremy.zip@gmail.com>
2024-05-25 16:39:40 +0200

    Fix task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,7 +2,7 @@
 """
     Class file for Rectangle.
 """
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
