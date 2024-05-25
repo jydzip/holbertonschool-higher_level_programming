7d670ea09628d531dcd2c9117cee7935211e88c0
jydzip <dckjeremy.zip@gmail.com>
2024-05-25 17:42:12 +0200

    Fix task 02

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -4,16 +4,16 @@
 class VerboseList(list):
     def append(self, el):
         print(f"Added [{el}] to the list.")
-        super().append(el)
+        return super().append(el)
 
     def extend(self, els):
         print(f"Extended the list with [{len(els)}] items.")
-        super().extend(els)
+        return super().extend(els)
 
     def remove(self, el):
         print(f"Removed [{el}] from the list.")
-        super().remove(el)
+        return super().remove(el)
 
     def pop(self, index=-1):
         print(f"Popped [{self[index]}] from the list.")
-        super().pop(index)
+        return super().pop(index)
