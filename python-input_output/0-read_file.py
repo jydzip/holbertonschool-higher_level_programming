b0e54321638176dde266d720d111482f246048f7
jydzip <dckjeremy.zip@gmail.com>
2024-05-27 09:05:19 +0200

    Add task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    File function for read_file().
+"""
+
+
+def read_file(filename=""):
+    """
+        Reads a text file (UTF8) and prints it to stdout.
+        Arguments:
+            filename (str): File to open.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
