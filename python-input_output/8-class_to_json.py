9085e310dc3a1b692a10aca6159326f67616c20b
jydzip <dckjeremy.zip@gmail.com>
2024-05-27 10:50:50 +0200

    Add task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,7 +2,6 @@
 """
     File function for class_to_json().
 """
-import json
 
 
 def class_to_json(obj):
@@ -10,6 +9,7 @@ def class_to_json(obj):
         Returns the dict description with simple data structure of an object.
         Arguments:
             obj: Object to list.
+        Return:
+            (dict): Dictionnary with values.
     """
-    _str = json.dumps(obj, default=lambda o: o.__dict__)
-    return json.loads(_str)
+    return obj.__dict__
