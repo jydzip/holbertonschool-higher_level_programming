ae2dc028fd665c40a897970f5343d2b093c69ff0
jydzip <dckjeremy.zip@gmail.com>
2024-05-28 09:27:44 +0200

    Fix task 2

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,13 +5,12 @@ import csv
 
 def convert_csv_to_json(filename):
     try:
-        OUTPUT = filename.replace(".csv", ".json")
         data = []
         with open(filename, "r") as fc:
             reader = csv.DictReader(fc)
             for row in reader:
                 data.append(row)
-        with open(OUTPUT, "w") as f:
+        with open("data.json", "w") as f:
             f.write(json.dumps(data, indent=4))
     except Exception:
         return False
