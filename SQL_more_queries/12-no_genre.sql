072abaf43f74a9dfb4548b4e2f4a2d3e56e3c1f0
jydzip <dckjeremy.zip@gmail.com>
2024-06-21 13:58:08 +0200

    Fix tasks

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,9 +2,9 @@
 SELECT
 	tv_shows.title, tv_show_genres.genre_id
 FROM
-	hbtn_0d_tvshows.tv_shows
+	tv_shows
 LEFT JOIN
-	hbtn_0d_tvshows.tv_show_genres
+	tv_show_genres
 		ON tv_shows.id = tv_show_genres.show_id
 WHERE
 	tv_show_genres.genre_id IS NULL
