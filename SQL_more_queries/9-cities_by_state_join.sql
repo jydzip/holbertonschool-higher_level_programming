cff662e28e70a00048d56a62587bf39e489f45af
jydzip <dckjeremy.zip@gmail.com>
2024-06-21 13:37:18 +0200

    Fix task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT
-	cities.id, cities.name, states.name
-FROM
-	hbtn_0d_usa.cities
-JOIN
-	hbtn_0d_usa.states
-		ON cities.state_id = states.id
-ORDER BY
-	cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
