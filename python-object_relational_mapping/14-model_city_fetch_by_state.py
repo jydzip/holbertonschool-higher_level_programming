682df081f9d8c0df706b3ed5aafe4a2a3de9d8a5
jydzip <dckjeremy.zip@gmail.com>
2024-06-23 14:00:14 +0200

    Fix task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,11 +3,11 @@
     Script that prints all City objects from the database.
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
 
-from model_state import City
+from model_state import Base, State
+from model_city import City
 
 
 if __name__ == "__main__":
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,16 +3,15 @@
     Models states file.
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+
+from model_state import Base
 
 
-Base = declarative_base()
-
-
-class State(Base):
+class City(Base):
     """
-        State class in the database.
+        City class in the database.
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(ForeignKey("states.id"), nullable=False)
