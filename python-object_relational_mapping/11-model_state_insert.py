1252de9486f5a5bec00eb923ac3973c2a5ad4479
jydzip <dckjeremy.zip@gmail.com>
2024-06-23 13:57:02 +0200

    Fix task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     session = Session(engine)
     new_state = State(name="Louisiana")
     session.add(new_state)
-    print("{:d}".format(new_state.id))
     session.commit()
+    print("{0}".format(new_state.id))
