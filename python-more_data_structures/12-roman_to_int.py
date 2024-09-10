28c42a4a084f90f3ea31495df526cbd244d03c12
ekoow <deckpich.zip@orange.fr>
2024-09-10 20:48:49 +0200

    Add task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    result = 0
     if not roman_string or not isinstance(roman_string, str):
-        return 0
+        return result
 
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+    _romans = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
     }
-    num = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            num += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
-        else:
-            num += roman_dict[roman_string[i]]
-    return num
+        letter = roman_string[i]
+        previous_letter = None
+        if i > 0:
+            try:
+                previous_letter = roman_string[i - 1]
+            except Exception:
+                previous_letter = None
+
+        r = _romans.get(letter, 0)
+        less = None
+        if previous_letter is not None:
+            less = _romans[previous_letter]
+        if less is not None and less < r:
+            result -= (less * 2)
+        result += r
+    return result
