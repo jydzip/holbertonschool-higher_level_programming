e372b412b25793dc75ae9f95f7f2a1e932ab9e4b
jydzip <dckjeremy.zip@gmail.com>
2024-10-04 15:05:26 +0200

    Add project 1 and 2

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-import csv
 import json
-from pyparsing import White
+import csv
 
 
-def convert_csv_to_json(csv_filename):
-    """
-    Converts a CSV file to a JSON file.
-    Returns True if successful, False if the CSV file is not found.
-    """
-    json_data = []
-
-    '''Open the CSV file for reading'''
-    with open(csv_filename, mode='r', encoding='utf-8') as csv_file:
-        '''Create a CSV reader object'''
-        csv_reader = csv.DictReader(csv_file)
-
-        '''Iterate through each row in the CSV file'''
-        for row in csv_reader:
-            json_data.append(row)
-
-    '''Write the JSON data to a file'''
-    with open('data.json', mode='w', encoding='utf-8') as json_file:
-        json.dump(json_data, json_file, indent=4)
+def convert_csv_to_json(filename):
+    try:
+        data = []
+        with open(filename, "r") as fc:
+            reader = csv.DictReader(fc)
+            for row in reader:
+                data.append(row)
+        with open("data.json", "w") as f:
+            f.write(json.dumps(data, indent=4))
+    except Exception:
+        return False
+    return True
