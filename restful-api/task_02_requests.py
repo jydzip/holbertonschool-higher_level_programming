1b1fa761c4977d82bae3302209121d05819b3d1e
jydzip <dckjeremy.zip@gmail.com>
2024-10-18 20:47:40 +0200

    Add project

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,25 +1,25 @@
 import requests
 import csv
 
-url = "https://jsonplaceholder.typicode.com/posts"
-
+url = 'https://jsonplaceholder.typicode.com/posts'
 
 def fetch_and_print_posts():
     response = requests.get(url)
-    print("Status code: ", response.status_code)
+    response_dict = response.json()
     if response.status_code == 200:
-        for line in response.json():
-            print(line["title"])
+        print(f'Status Code: {response.status_code}')
+        for post in response_dict:
+            print(post['title'])
 
 def fetch_and_save_posts():
     response = requests.get(url)
     if response.status_code == 200:
-        fieldnames = ["id", "body", "title"]
-        with open("posts.csv", "w") as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            for line in response.json():
-                writer.writerow({
-                    'id': line['id'],
-                    'body': line['body'],
-                    'title': line['title']
-                })
+        response_dict = response.json()
+
+        with open('posts.csv', mode='w', newline='') as f:
+            fieldnames = ['id', 'title', 'body']
+            csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+            csv_writer.writeheader()
+            
+            for post in response_dict:
+                csv_writer.writerow({'id': post['id'], 'title': post['title'], 'body': post['body']})
